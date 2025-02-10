ec75b13face2c1fd9eea123832af62dad3a6bf40
Robin-Kurian <dev.robinkurian@gmail.com>
2025-02-10 07:40:34 +0530

    Remove carousel from home page

--- app/page.js
+++ app/page.js
@@ -1,22 +1,14 @@
-import AutoplayCarousel from "@/components/common/AutoplayCarousel";
 import ContentWrapper from "@/components/common/layouts/ContentWrapper";
 import HomeComponent from "@/components/HomeComponent";
-import CAROUSEL_API from "@/utilities/api/carousel.api";
 import BLOG_API from "@/utilities/api/blog.api";
 export default async function Home() {
-  const [carouselResponse, blogsResponse] = await Promise.all([
-    CAROUSEL_API.getCarousel(),
-    BLOG_API.getBlogs(),
-  ]);
+  const [blogsResponse] = await Promise.all([BLOG_API.getBlogs()]);
 
-  const { data: CAROUSEL } = carouselResponse;
   const { data: BLOGS } = blogsResponse;
   const INITIAL_DATA = { BLOGS };
 
   return (
     <ContentWrapper className="p-1 gap-4">
-      {/* CAROUSEL SECTION */}
-      {CAROUSEL.length > 0 && <AutoplayCarousel items={CAROUSEL} />}
       <HomeComponent initialData={INITIAL_DATA} />
     </ContentWrapper>
   );
