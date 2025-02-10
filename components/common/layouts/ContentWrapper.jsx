2abbcbdabf0aab25f10709cc9706d7d1a5bf439e
Robin-Kurian <dev.robinkurian@gmail.com>
2025-02-10 07:40:25 +0530

    Update ContentWrapper with left margin styling

--- components/common/layouts/ContentWrapper.jsx
+++ components/common/layouts/ContentWrapper.jsx
@@ -1,6 +1,8 @@
 // CONTENTWRAPPER FOR WRAPPING THE CONTENTS WITH COMMON STYLING
 const ContentWrapper = ({ children, className = "" }) => {
-  return <div className={`flex flex-col w-full ${className}`}>{children}</div>;
+  return (
+    <div className={`flex flex-col w-full ml-2 ${className}`}>{children}</div>
+  );
 };
 
 export default ContentWrapper;
