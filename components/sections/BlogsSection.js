37ee60d21980450903008f64e4a8defcd817b510
Robin-Kurian <dev.robinkurian@gmail.com>
2025-02-13 05:56:26 +0530

    and contact section, update home and blog sections

--- components/sections/BlogsSection.js
+++ components/sections/BlogsSection.js
@@ -2,21 +2,21 @@ import Gridlayout from "@/components/common/layouts/Gridlayout";
 import BlogCard from "@/components/user/BlogCard";
 
 const BlogsSection = ({ blogs, onBlogClick }) => {
-    return (
-        <section id="blogs" className="min-h-full py-20">
-            <h2 className="text-4xl font-bold mb-8">Latest Blogs</h2>
-            <Gridlayout>
-                {blogs.map((blog) => (
-                    <BlogCard
-                        key={blog?.id}
-                        onClick={() => onBlogClick(blog)}
-                        blog={blog}
-                        className="w-full"
-                    />
-                ))}
-            </Gridlayout>
-        </section>
-    );
+  return (
+    <section id="blogs" className="min-h-full py-20">
+      <h2 className="text-4xl font-bold mb-8">Blogs</h2>
+      <Gridlayout>
+        {blogs.map((blog) => (
+          <BlogCard
+            key={blog?.id}
+            onClick={() => onBlogClick(blog)}
+            blog={blog}
+            className="w-full"
+          />
+        ))}
+      </Gridlayout>
+    </section>
+  );
 };
 
-export default BlogsSection; 
+export default BlogsSection;
