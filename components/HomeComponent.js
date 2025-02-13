56746143dcadc23caa8c1bd9933e28e8d901a1ff
Robin-Kurian <dev.robinkurian@gmail.com>
2025-02-13 05:55:54 +0530

    Add scroll-to-top button

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -12,16 +12,40 @@ import SkillsSection from "@/components/sections/SkillsSection";
 import WorksSection from "@/components/sections/WorksSection";
 import GallerySection from "@/components/sections/GallerySection";
 import BlogsSection from "@/components/sections/BlogsSection";
+import ContactSection from "@/components/sections/ContactSection";
+import { Button } from "@heroui/react";
+import { FiArrowUp } from "react-icons/fi";
 
 const HomeComponent = ({ initialData }) => {
   const { blogs, setBlogs } = useBlogStore();
   const [isLoading, setIsLoading] = useState(true);
   const setBlogDetails = useBlogStore((state) => state.setBlogDetails);
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
 
   useEffect(() => {
     setBlogs(initialData.BLOGS);
     setIsLoading(false);
   }, [initialData.BLOGS, setBlogs]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      // Show button when scrolled down to 30% of the page
+      if (scrollPosition > (documentHeight - windowHeight) * 0.3) {
+        setIsButtonVisible(true);
+      } else {
+        setIsButtonVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleBlogClick = (blog) => {
     console.log("clicked");
@@ -38,7 +62,23 @@ const HomeComponent = ({ initialData }) => {
       <ExperienceSection />
       <WorksSection />
       <BlogsSection blogs={blogs} onBlogClick={handleBlogClick} />
-      <GallerySection />
+      {/* <GallerySection /> */}
+      <ContactSection />
+      {isButtonVisible && (
+        <div className="flex justify-end fixed bottom-10 right-8">
+          <Button
+            radius="full"
+            isIconOnly
+            onPress={() => {
+              document
+                .getElementById("home")
+                .scrollIntoView({ behavior: "smooth" });
+            }}
+          >
+            <FiArrowUp />{" "}
+          </Button>
+        </div>
+      )}
     </ContentWrapper>
   );
 };
