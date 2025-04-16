557ad60d07e8bf204844d0515bb70cd1489e0da3
Robin-Kurian <dev.robinkurian@gmail.com>
2025-04-16 16:33:22 +0530

    Remove unused files and configurations: Deleted .firebaserc, various JSON, CSS, and JavaScript files, as well as images and utility functions to clean up the project structure.

--- next.config.mjs
+++ next.config.mjs
@@ -1,57 +1,13 @@
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        pathname: "/v0/b/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-      {
-        source: "/sw.js",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/javascript; charset=utf-8",
-          },
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
--- utilities/urls.js
+++
@@ -1,4 +0,0 @@
-export const URLS = {
-    POSTAL_API_URL: "https://api.postalpincode.in/pincode/",
-    // GOOGLE_MAPS_API_URL: "https://maps.googleapis.com/maps/api/geocode/json",
-}
